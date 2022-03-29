d4b6d95e6a610ddb83ce59b24f01ed022bdf83b2
Jose Jaime Gutierrez <josejaimegutierrez@Joses-MacBook-Pro-2.local>
2022-03-29 09:57:52 -0500

    update

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import image from "../img/backgi.jpeg";
+//import img from "../img/vid.png"
+import { Link } from "react-router-dom";
+
+class Home extends Component {
+    render() { 
+        return (
+            <div style={styles.container}>
+                <div>
+                    <Link to="/Video"><div style={styles.button}><div style={styles.vid}></div></div></Link>
+                    <Link to="/Message"><div style={styles.button}></div></Link>
+                    <Link to="/Microsoft"><div style={styles.button}></div></Link>
+                </div>
+                <div>
+                    <Link to="/Archive"><div style={styles.button}></div></Link>
+                    <Link to="/Learning"><div style={styles.button}></div></Link>
+                </div>
+            </div>
+        );
+    }
+};
+ 
+const styles ={
+    container: {
+        backgroundImage: `url(${image})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100vw',
+        height: '100vh',
+    },
+    button: {
+        border: 4,
+        borderColor:"black",
+        backgroundColor: "silver",
+        color: "black",
+        paddingTop: "7%",
+        paddingBottom: "7%",
+        width: 300,
+        textalign: "center", 
+        textdecoration: "none",
+        display: "inline-block",
+        borderRadius: 20,
+        borderColor: "black",
+        marginTop: "9%",
+        marginLeft: "9%"
+    },
+    vid: {
+        //backgroundImage: `url(${img})`,
+    },
+    
+    
+};
+export default Home;
