add6795cf02af3c71c9c9c57ee937940bd1e7ccd
Andree Angulo <58123107+AndreeAng@users.noreply.github.com>
2022-03-30 00:28:34 -0500

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,81 @@
-import React from "react";
+import React from 'react'
+import { useRef, useState, useEffect } from 'react';
 
-function Login(){
-    return <div> THIS IS THE LOGIN PAGE </div>;
+const Login = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState('');
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd])
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        console.log(user, pwd);
+        setUser('');
+        setPwd('');
+        setSuccess(true);
+    }
+
+    
+  return (
+    <>
+        {success ? (
+            <section>
+                <h1>You are logged in!</h1>
+                <br />
+                <p>
+                    <a href='#'>Go to Home</a>
+                </p>
+            </section>
+        ) : (
+
+    <section>
+        <p ref={errRef} className={errMsg ? "errmsg" :
+        "offscreen"} aria-live="assertive">{errMsg}</p>
+        <h1>Sign In</h1>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor='username'>Username:</label>
+            <input 
+                type='text' 
+                id='username'
+                ref={userRef}
+                autoComplete='off'
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+            />
+            
+            <label htmlFor='password'>Password:</label>
+            <input 
+                type='password' 
+                id='password'
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+            />
+            <button>Sign In</button>
+        </form>
+        <p>
+            Need an Account?<br />
+            <span className='line'>
+                {/*put router link here*/}
+                <a href='#'>Sign Up</a>
+            </span>
+        </p>
+    </section>
+        )}
+        </>
+  )
 }
 
-export default Login;
+export default Login
