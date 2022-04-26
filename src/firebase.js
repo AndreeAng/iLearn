cd2beed4507ab3716f3c1c0944d879991e3f8c2e
Jose Jaime Gutierrez <josejaimegutierrez@Joses-MacBook-Pro-2.local>
2022-04-25 21:10:15 -0500

    update

--- src/firebase.js
+++ src/firebase.js
@@ -12,13 +12,12 @@ import {
   addDoc,
 } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyDIXJ5YT7hoNbBFqK3TBcV41-TzIO-7n7w",
-  authDomain: "fir-auth-6edd8.firebaseapp.com",
-  projectId: "fir-auth-6edd8",
-  storageBucket: "fir-auth-6edd8.appspot.com",
-  messagingSenderId: "904760319835",
-  appId: "1:904760319835:web:44fd0d957f114b4e51447e",
-  measurementId: "G-Q4TYKH9GG7",
+  apiKey: "AIzaSyBmuCNOnujpsM47l8TZ9_7hUg8_l72EAZI",
+  authDomain: "ilearn-93636.firebaseapp.com",
+  projectId: "ilearn-93636",
+  storageBucket: "ilearn-93636.appspot.com",
+  messagingSenderId: "879637857088",
+  appId: "1:879637857088:web:adfdad5c447ee0bd4ff0a5"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
